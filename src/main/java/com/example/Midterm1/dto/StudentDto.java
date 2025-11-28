0969353c4724f2ff3faf5ba8324755d5a1fac6fe
Sandibala <sandibalaamze17@gmail.com>
2025-11-28 12:23:47 +0500

    Update StudentDto

--- src/main/java/com/example/Midterm1/dto/StudentDto.java
+++ src/main/java/com/example/Midterm1/dto/StudentDto.java
@@ -1,8 +1,4 @@
 package com.example.Midterm1.dto;
-
-import com.example.Midterm1.models.Book;
-import com.example.Midterm1.models.Student;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
